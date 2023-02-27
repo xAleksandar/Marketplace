db2b5b4e573c511a5f85960bfa603f49e4ed4de6
alex <aleksandar@kalaydzhiev.com>
2023-02-27 12:09:38 +0200

    Remove unused functions.

--- src/hooks/usеNFTManager.tsx
+++ src/hooks/usеNFTManager.tsx
@@ -40,140 +40,6 @@ const useNFTManager = (marketplace: Contract, signer: JsonRpcSigner, initialUser
         }
     })
 
-    const mintNFT = async (collectionId: number, image: string, changeModalState: (state:number) => void, setTx: (tx: string) => void) => {
-        const transaction = await marketplace.mintNFT(collectionId, image);
-        changeModalState(2)
-        setTx(transaction.hash)
-        
-        marketplace.once("nft", async (action, id, issuer) => {
-            if (initialUser.toString().toLowerCase() == issuer.toLowerCase() && action === "Mint") {
-            changeModalState(3)
-            }
-        })
-        }
-
-
-
-    const buyNFT = async (marketItem: marketNFT, changeModalState: (state:number) => void, setTx: (tx: string) => void) => {
-        const totalitems = (await marketplace.returnItemsLength()).toString()
-        
-        for (let i = 0; i <=totalitems; i++) {
-            let nft = await marketplace.items(i)
-            let nftid = nft.tokenId.toString()
-            let nftaddress = nft.nft
-            let price = nft.price
-            
-            if (nftid == marketItem.tokenId && nftaddress == marketItem.collection) {
-                const transaction = await marketplace.buyNFT(i, { value: price})
-                changeModalState(2)
-                setTx(transaction.hash);
-                marketplace.once("nft", async (action, id, issuer) => {
-                    if(action === "Buy") {
-                        changeModalState(3)
-                    }
-                })
-            }
-        }
-
-        marketItem.forSell = false;
-        return marketItem;
-    } 
-
-
-
-    const unlistNFT = async (marketItem: marketNFT, changeModalState: (state:number) => void, setTx: (tx: string) => void) => {
-        const totalitems = (await marketplace.returnItemsLength()).toString()
-        
-        for (let i = 0; i <=totalitems; i++) {
-            let nft = await marketplace.items(i)
-            let nftid = nft.tokenId.toString()
-            let nftaddress = nft.nft
-            let price = nft.price
-            
-            if (nftid == marketItem.tokenId && nftaddress == marketItem.collection) {
-                const transaction = await marketplace.cancelSell(i);
-                changeModalState(2)
-                setTx(transaction.hash);
-                marketplace.once("nft", async (action, id, issuer) => {
-                    if(action === "CancelSell") {
-                        changeModalState(3)
-                    }
-                })
-            }
-        }
-
-        marketItem.forSell = false;
-        return marketItem;
-    } 
-
-
-
-    const bidForNFT = async (marketItem: marketNFT, price: string, changeModalState: (state:number) => void, setTx: (tx: string) => void) => {
-
-        const totalitems: number = parseInt((await marketplace.returnItemsLength()).toString())
-        const weiprice: number = parseInt(ethers.utils.parseEther(price).toString())
-        
-        for (let i = 0; i <= totalitems; i++) {
-            let nft = await marketplace.items(i)
-            let nftid = nft.tokenId.toString()
-            let nftaddress = nft.nft
-            
-            if (nftid == marketItem.tokenId && nftaddress == marketItem.collection) {
-                const transaction = await marketplace.bidOnNFT(i, { value: weiprice});
-                changeModalState(2)
-                setTx(transaction.hash);
-                marketplace.once("nft", async (action, id, issuer) => {
-                    if(action === "Bid") {
-                        changeModalState(3)
-                    }
-                })
-            }
-        }
-
-        marketItem.bidPrice = price;
-        return marketItem
-    }
-
-
-
-    const sellNFT = async (item: marketNFT, price: string, changeModalState: (state:number) => void, setTx: (tx: string) => void) => {
-        const weiprice = ethers.utils.parseEther(price)
-        const totalitems = parseInt((await marketplace.returnItemsLength()).toString())
-        
-        for (let i = 0; i <=totalitems; i++) {
-            let nft = await marketplace.items(i)
-            let nftid = nft.tokenId.toString()
-            let nftaddress = nft.nft
-            
-            if (nftid == item.tokenId && nftaddress == item.collection) {
-                const transaction = await marketplace.sellNFT(i, weiprice)
-                setTx(transaction.hash)
-                changeModalState(2)
-                marketplace.once("nft", async (action, id, issuer) => {
-                    if(action === "Sell") {
-                        changeModalState(3)
-                    }
-                })
-            }
-        }
-    }
-
-
-
-    const acceptBid = async (item: marketNFT) => {
-        const totalitems = parseInt((await marketplace.returnItemsLength()).toString())
-
-        for (let i = 0; i <= totalitems; i++) {
-            let nft = await marketplace.items(i)
-            let nftid = nft.tokenId.toString()
-            let nftaddress = nft.nft
-            
-            if (nftid == item.tokenId && nftaddress == item.collection) {
-                await marketplace.acceptBid(i)
-            }
-        }
-    }
-
 
 
     const fetchUserItems = async (address: string, forSell: boolean) => {
@@ -219,30 +85,13 @@ const useNFTManager = (marketplace: Contract, signer: JsonRpcSigner, initialUser
 
 
 
-    const approveMarketplace = async (marketItem: marketNFT) => {
-        const NFTcontract = new ethers.Contract(marketItem.collection, NFTAbi.abi, signer)
-        await NFTcontract.approve(MarketplaceAddress.address, marketItem.tokenId)
-        marketItem.isMarketplaceApproved = true;
-        console.log('MarketItem: ',)
-        return marketItem
-    }
 
-
-
-    return {mintNFT,
-            buyNFT,
-            unlistNFT, 
-            bidForNFT, 
-            nftTrigger,
+    return { nftTrigger,
             fetchAllItems,
             items,
             loading,
-            zeroItems,
             setUserAddress,
-            setTrigger,
-            approveMarketplace,
-            sellNFT,
-            acceptBid}
+            setTrigger}
 
 }
 
